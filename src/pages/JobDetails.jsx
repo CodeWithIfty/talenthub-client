938221451b03306e22a983011027c2eb3e70a5cf
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-11-06 19:26:42 +0600

    bidder validation user can't bid on their own job

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -2,11 +2,14 @@ import { useLoaderData, useParams } from "react-router-dom";
 import ApplyJobForm from "../components/ApplyJobForm";
 import useAxios from "../utils/hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { authContext } from "../utils/context/AuthProvider";
 
 const JobDetails = () => {
   const { _id } = useParams();
   console.log(_id);
   const axios = useAxios();
+  const { user } = useContext(authContext);
 
   const {
     data: job,
@@ -24,8 +27,15 @@ const JobDetails = () => {
     },
   });
   console.log(job);
-  const { jobTitle, deadline, description, maximumPrice, minimumPrice } =
-    job || {};
+  const {
+    jobTitle,
+    deadline,
+    jobDescription,
+    minPrice,
+    maxPrice,
+    clientInfo,
+    category,
+  } = job || {};
   return (
     <div>
       <div className="mt-24 bg-[#12CD6A] p-10">
@@ -36,20 +46,36 @@ const JobDetails = () => {
 
       <div className="border-b-2">
         <div className="w-7/12 mx-auto flex p-5 justify-between">
-          <div className="">
-            <h1>Ascend Consulting</h1>
+          <div className="flex items-center gap-2">
+            <div className=" ">
+              <img
+                src={`${
+                  clientInfo?.photoURL
+                    ? clientInfo?.photoURL
+                    : "https://i.ibb.co/rHzPb0S/icon-256x256.png"
+                }`}
+                alt={clientInfo?.displayName}
+                className="w-14 h-14 rounded-full"
+              />
+            </div>
+            <div className="">
+              <h1 className="font-bold text-xl">{clientInfo?.displayName} </h1>
+              <p className="text-sm italic">{clientInfo?.email}</p>
+            </div>
           </div>
           <div className="">
-            <p>Sallary</p>
-            <h2>£25,000 - £30,000</h2>
+            <p className="font-bold">Price range</p>
+            <h2>
+              ${minPrice} - ${maxPrice}
+            </h2>
           </div>
           <div className="">
-            <p>Hours</p>
-            <h2>44h / week</h2>
+            <p className="font-bold">Deadline</p>
+            <h2>{deadline}</h2>
           </div>
           <div className="">
-            <p>Locations</p>
-            <h2>West London</h2>
+            <p className="font-bold">Category</p>
+            <h2>{category}</h2>
           </div>
         </div>
       </div>
@@ -58,40 +84,34 @@ const JobDetails = () => {
         <div className="w-10/12  p-10">
           <div className="">
             <h1 className="text-5xl font-semibold mb-3">{jobTitle}</h1>
-            <p className="w-10/12 ml-5">
-              You will help Google build next-generation web applications like
-              Gmail, Google Docs, Google Analytics, and the Google eBookstore
-              and eBook readers. As a Front End Engineer at Google, you will
-              specialize in building responsive and elegant web UIs with AJAX
-              and similar technologies. You may design or work on frameworks for
-              building scalable frontend applications. We are looking for
-              engineers who are passionate about and have experience building
-              leading-edge user experience, including dynamic consumer
-              experiences.
-            </p>
-          </div>
-          <div className="">
-            <h1 className="text-3xl mt-5 font-semibold">Good to have:</h1>
-            <ul className="list-disc ml-10">
-              <li>Wordpress</li>
-              <li>Wordpress</li>
-              <li>Wordpress</li>
-              <li>Wordpress</li>
-            </ul>
+            <p className="w-10/12 ml-5">{jobDescription}</p>
           </div>
         </div>
         <div className="w-2/12 bg-gray-600 bg-opacity-10 rounded-lg">
-          <div className="p-10 flex flex-col justify-center items-center">
-            <button
-              onClick={() => document.getElementById("my_modal_3").showModal()}
-              className="py-2 px-4 border-2 border-success  rounded-full hover:bg-[#12CD6A] hover:shadow-xl font-semibold hover:text-white transition duration-200 ease-in-out"
-            >
-              PLACE YOUR BID
-            </button>
-            <p className="text-sm text-center mt-2 italic">
-              Click here to open bid form
-            </p>
-          </div>
+          {user.email === clientInfo?.email ? (
+            <div className="p-10 flex flex-col justify-center items-center">
+              <input
+                readOnly
+                disabled
+                value={"You Can't Bid"}
+                className="py-2 text-center border-2 border-success  rounded-full font-semibold  transition duration-200 ease-in-out"
+              />
+            </div>
+          ) : (
+            <div className="p-10 flex flex-col justify-center items-center">
+              <button
+                onClick={() =>
+                  document.getElementById("my_modal_3").showModal()
+                }
+                className="py-2 px-4 border-2 border-success  rounded-full hover:bg-[#12CD6A] hover:shadow-xl font-semibold hover:text-white transition duration-200 ease-in-out"
+              >
+                PLACE YOUR BID
+              </button>
+              <p className="text-sm text-center mt-2 italic">
+                Click here to open bid form
+              </p>
+            </div>
+          )}
         </div>
       </div>
 
